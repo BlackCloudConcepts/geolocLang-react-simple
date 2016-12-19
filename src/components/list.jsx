c21f6f799808653c4839974fc4dab18e45ae7592
blackcloudconcepts <blackcloudconcepts@gmail.com>
2016-12-19 13:56:22 -0500

    handle observable in list

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -8,12 +8,6 @@ class List extends React.Component {
   constructor(props) {
     super(props);
     this.props = props;
-    this.state = {
-      word1: 'a',
-      word2: 'b',
-      word3: 'c'
-    };
-
   }
  
   render() {
@@ -23,13 +17,13 @@ class List extends React.Component {
           List of Words
         </div>
         <div>
-          {this.state.word1}        
+          {this.props.word1}        
         </div>
         <div>
-          {this.state.word2}
+          {this.props.word2}
         </div>
         <div>
-          {this.state.word3}
+          {this.props.word3}
         </div>
       </div>
     );
@@ -41,10 +35,21 @@ List.propTypes = {
 }
 
 const mapStateToProps = (state) => {
+  var obj = state.getIn(['locations', 0]);
+  var ste = {
+    word1: '',
+    word2: '',
+    word3: ''
+  }
+  if (obj){
+    ste.word1 = obj.word1;
+    ste.word2 = obj.word2;
+    ste.word3 = obj.word3;
+  }
   return {
-    word1: state.get('word1'),
-    word2: state.get('word2'),
-    word3: state.get('word3'),
+    word1: ste.word1,
+    word2: ste.word2,
+    word3: ste.word3
   }
 }
 
--- src/components/word-input.jsx
+++ src/components/word-input.jsx
@@ -8,24 +8,27 @@ class WordInput extends React.Component {
   constructor(props) {
     super(props);
     this.props = props;
+/*
     this.state = {
       word1: '',
       word2: '',
       word3: ''
     };
-
     this.handleChange = this.handleChange.bind(this);
-
+*/
   }
- 
+
+/* 
   handleChange(event) {
     this.setState({[event.target.id]: event.target.value});
   }
+*/
 
   render() {
     const { handleClick } = this.props;
     return (
       <div className={styles.card}>
+{/*
         <div>
           <input type="text" id="word1" size="50" onChange={this.handleChange} value={this.state.word1} />          
         </div>
@@ -34,6 +37,16 @@ class WordInput extends React.Component {
         </div>
         <div>
           <input type="text" id="word3" size="50" onChange={this.handleChange} value={this.state.word3} />          
+        </div>
+*/}
+        <div>
+          <input type="text" id="word1" size="50" />          
+        </div>
+        <div>
+          <input type="text" id="word2" size="50" />          
+        </div>
+        <div>
+          <input type="text" id="word3" size="50" />          
         </div>
         <div>
           <button onClick={handleClick}>Find</button>
@@ -60,11 +73,14 @@ WordInput.propTypes = {
 }
 
 const mapStateToProps = (state) => {
+  return null;
+/*
   return {
     word1: state.get('word1'),
     word2: state.get('word2'),
     word3: state.get('word3'),
   }
+*/
 }
 
 
@@ -82,7 +98,7 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 export const WordInputContainer = connect(
-  mapStateToProps,
+  null,
   mapDispatchToProps
 )(WordInput)
 
